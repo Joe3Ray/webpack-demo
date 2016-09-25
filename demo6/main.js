bae38a68542b989b3844eaf67aa9fbdc6f4f955c
JoeRay61 <joeray199261@gmail.com>
2016-09-25 11:15:13 +0800

    add: demo6 files

---
+++ demo6/main.js
@@ -0,0 +1,7 @@
+var img1 = new Image();
+img1.src = require('./webpack.png');
+document.body.appendChild(img1);
+
+var img2 = new Image();
+img2.src = require('./wade.jpg');
+document.body.appendChild(img2);
